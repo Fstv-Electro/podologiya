39e0708524fcbfd432fbba35cd5639a065a12f87
Fstv-Electro <104622826+Fstv-Electro@users.noreply.github.com>
2022-12-18 14:23:55 +0000

    Deploying to gh-pages from @ Fstv-Electro/podologiya@cfab62766e76ce669b6c1a40d2dfc1845a351c23 🚀

---
+++ index.8c6a8073.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector(".js-open-btn"),closeModalBtn:document.querySelector(".js-close-btn"),modal:document.querySelector("[data-menu]"),modalList:document.querySelector(".header-nav__list--mobile")};function t(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",t),e.closeModalBtn.addEventListener("click",t),e.modalList.addEventListener("click",t)})();
+//# sourceMappingURL=index.8c6a8073.js.map
